2f3896fb51a02288845477ca3482b0bf59a799cd
= <=>
2019-11-19 18:51:27 +0700

    Added feature: console application

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to use this code
+This project is intended for my self-study purpose. However, you guys can use it for building industry-class products.
+
+### What is included:
+- How to build Spring Boot console application
+- How to work with multiple MongoDB connector
+- How to produce/consume Kafka message
+
+### Enjoy learning!!!
--- src/main/java/com/goldengate/springbootconsoleapp/SpringBootConsoleAppApplication.java
+++ src/main/java/com/goldengate/springbootconsoleapp/SpringBootConsoleAppApplication.java
@@ -1,13 +1,20 @@
 package com.goldengate.springbootconsoleapp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class SpringBootConsoleAppApplication {
+public class SpringBootConsoleAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootConsoleAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Hello World!!!");
+	}
 }
